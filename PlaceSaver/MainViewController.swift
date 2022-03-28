aa8e23c768085d92022863e5953163dc0e749161
Anton Balashov <za>
2022-03-28 14:26:45 +0300

    Initial

---
+++ PlaceSaver/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  PlaceSaver
+//
+//  Created by MacBookAnt on 24.03.2022.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let restaurantNames = ["hydra", "jul", "kfc", "turandot", "twins", "mc", "Zhivag", "sempre", "fish"]
+     
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+    // MARK: - Table view data source
+
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+    
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
